fd01822a2429499532b79e2dd4d40d651f177ff5
Alfred <109942648+khangduy0340@users.noreply.github.com>
2023-03-10 22:51:46 +1030

    Add files via upload

--- Referee.cpp
+++ Referee.cpp
@@ -16,9 +16,8 @@ Player* Referee::refGame(Player *player1, Player *player2){
         // Case move2 inside move1 winning list
         // move1 win -> player1 win
         if(winner){delete move1;delete move2;return player1;};
+
         
-        delete move1;
-        delete move2;
         // As if those two conditions are not passed then player 2 is won
         return player2;
 }
