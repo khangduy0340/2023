4ad39629b83192ecfc66495a55f767447c9e50f1
Alfred <109942648+khangduy0340@users.noreply.github.com>
2023-05-20 17:04:33 +0930

    Add files via upload

--- klargest.cpp
+++ klargest.cpp
@@ -1,14 +1,19 @@
 #include <vector>
 #include <queue>
+
 using namespace std;
 
 int kth_largest(vector<int> values, int k){
-    // using priority sorting from low to high
+    // create a priority storing from smallest to largest
     priority_queue<int ,vector<int>, greater<int> > m;
-    // push the number of values according to int k
+    // push value to an array according to int k
     for (int value : values) {
     m.push(value);
+    // if size is bigger than k, delete the smallest number
+    if (m.size() > k) {
+      m.pop();
     }
-    // return the largest
+  }
+  // return the largest no
   return m.top();
 }
